27c6866f40b89b9c403065c046a42c29559fab7c
Robert <r.pustulka@zano.pl>
2015-07-29 15:12:56 +0200

    behavior added + composer updated

---
+++ src/Model/Behavior/StiBehavior.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Robotusers\TableInheritance\Model\Behavior;
+
+use ArrayAccess;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\ORM\Behavior;
+use Cake\ORM\Query;
+
+class StiBehavior extends Behavior
+{
+    const OPTION_NAME = 'discriminator';
+
+    /**
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'discriminatorField' => static::OPTION_NAME
+    ];
+
+    /**
+     *
+     * @param string $discriminator
+     * @return string
+     */
+    public function discriminator($discriminator = null)
+    {
+        if ($discriminator !== null) {
+            $this->_discriminator = $discriminator;
+        }
+        if ($this->_discriminator === null) {
+            $this->_discriminator = $this->alias();
+        }
+        return $this->_discriminator;
+    }
+
+    /**
+     * 
+     * @param Event $event
+     * @param EntityInterface $entity
+     * @param ArrayAccess $options
+     */
+    public function beforeSave(Event $event, EntityInterface $entity, ArrayAccess $options)
+    {
+        $discriminator = $this->_discriminator($options);
+
+        if ($discriminator !== false) {
+            $entity->set($this->_config['discriminatorField'], $discriminator);
+        }
+    }
+
+    /**
+     *
+     * @param Event $event
+     * @param Query $query
+     * @param ArrayAccess $options
+     */
+    public function beforeFind(Event $event, Query $query, ArrayAccess $options)
+    {
+        $discriminator = $this->_discriminator($options);
+
+        if ($discriminator !== false) {
+            $query->where([
+                $this->aliasField($this->_config['discriminatorField']) => $discriminator
+            ]);
+        }
+    }
+
+    /**
+     *
+     * @param ArrayAccess $options
+     * @return string
+     */
+    protected function _discriminator($options)
+    {
+        return $options->offsetExists(static::OPTION_NAME) ? $options[static::OPTION_NAME] : $this->discriminator();
+    }
+}
